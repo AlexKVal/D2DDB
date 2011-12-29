cd0333de8de4e6b30872ab1c2688e347d9e95297
AlexKVal <alexkval@gmail.com>
2011-12-29 21:44:58 +0200

    values strings for Insert and Update statements

--- lib/applier.rb
+++ lib/applier.rb
@@ -1,5 +1,6 @@
 require_relative 'config'
 require_relative 'central/received_data_queue'
+require_relative 'shared/pvsw'
 
 module Central
   class Applier
@@ -9,16 +10,44 @@ module Central
 
     def run
       received_rows = @received_data_queue.data
-      
+
       received_rows.each do |row|
         case row.action
         when 'I'
+          do_sql
         when 'U'
         when 'D'
         end
       end
-      
+
       @received_data_queue.clear!
     end
+
+    private
+      def names_values_for_insert_from(jsondata)
+        columns_hash = JSON.parse(jsondata)
+        names, values = [], []
+
+        columns_hash.each do |key, val|
+          names  << "'#{key}'"
+          values << (val.kind_of?(String) ? "'#{val}'" : val)
+        end
+        [names, values]
+      end
+
+      # "(columns) values (vals)"
+      def columns_for_insert(jsondata)
+        names, values = names_values_for_insert_from(jsondata)
+        "(#{names.join(', ')}) VALUES (#{values.join(', ')})"
+      end
+      
+      def columns_for_update(jsondata)
+        columns_hash = JSON.parse(jsondata)
+        columns_hash.shift # remove id column
+
+        columns_hash.inject([])  do |sets, (k,v)|
+          sets << "SET #{k} = #{String === v ? "'#{v}'" : v}"
+        end.join(', ')
+      end
   end
 end
--- spec/applier_spec.rb
+++ spec/applier_spec.rb
@@ -4,6 +4,60 @@ module Central
   describe Applier do
     let(:applier) {Applier.new}
 
+    describe "#names_values_for_insert_from" do
+      it "quotes column names" do
+        json_data = '{"col1":1,"col2":2}'
+        names, values = applier.send(:names_values_for_insert_from, json_data)
+        names.should eq %w('col1' 'col2')
+      end
+
+      it "quotes string values" do
+        json_data = '{"string1":"val1","string2":"val2"}'
+        names, values = applier.send(:names_values_for_insert_from, json_data)
+        values.should eq %w('val1' 'val2')
+      end
+
+      it "quotes Date and Time values" do
+        json_data = '{"date":"2011-11-12","time":"00:12:01"}'
+        names, values = applier.send(:names_values_for_insert_from, json_data)
+        values.should eq %w('2011-11-12' '00:12:01')
+      end
+
+      it "doesn't quotes integer (and logical) values" do
+        json_data = '{"integer":123,"logical":1}'
+        names, values = applier.send(:names_values_for_insert_from, json_data)
+        values.should eq [123, 1]
+      end
+    end
+
+    describe "#columns_for_insert" do
+      it "compilates a value part of Insert command from json_data" do
+        json_data = '{"str":"strval","int":2,"date":"11/12/2011","time":"00:12:00","bool":0}'
+        res = applier.send(:columns_for_insert, json_data)
+        res.should eq "('str', 'int', 'date', 'time', 'bool') VALUES ('strval', 2, '11/12/2011', '00:12:00', 0)"
+      end
+    end
+
+    describe "#columns_for_update" do
+      it "quotes string values" do
+        json_data = '{"PersID":23,"str":"strval"}'
+        res = applier.send(:columns_for_update, json_data)
+        res.should eq "SET str = 'strval'"
+      end
+
+      it "doesn't quotes integer (and logical) values" do
+        json_data = '{"PersID":23,"int":2}'
+        res = applier.send(:columns_for_update, json_data)
+        res.should eq "SET int = 2"
+      end
+
+      it "compilates a value part of Update command w/o ID column" do
+        json_data = '{"PersID":23,"str":"strval","int":2,"date":"11/12/2011","time":"00:12:00","bool":0}'
+        res = applier.send(:columns_for_update, json_data)
+        res.should eq "SET str = 'strval', SET int = 2, SET date = '11/12/2011', SET time = '00:12:00', SET bool = 0"
+      end
+    end
+
     describe "#run", :pvsw do
       after(:each) do
         ReceivedDataRow.clear!
@@ -14,7 +68,7 @@ module Central
         Pvsw.do_sql_single_result("SELECT COUNT(*) FROM tableTwo")
       end
 
-      it "runs sql commands with received data" do
+      xit "runs sql commands from the queue and then clean it" do
         pvsw_table_two_size.should eq 0
 
         # Insert
@@ -36,7 +90,15 @@ module Central
         applier.run
         pvsw_table_two_size.should eq 0
       end
+
+      it "cleans the queue after job done" do
+        ReceivedDataRow.create!(
+          tblname: 'tableTwo', rowid: 23, action: 'I',
+        data: '{"ID":23,"string_prm":"value","integer_prm":123,"date_prm":"2011-12-12"}')
+        ReceivedDataRow.all.size.should eq 1
+        applier.run
+        ReceivedDataRow.all.size.should eq 0
+      end
     end
   end
 end
-#string_prm integer_prm date_prm
